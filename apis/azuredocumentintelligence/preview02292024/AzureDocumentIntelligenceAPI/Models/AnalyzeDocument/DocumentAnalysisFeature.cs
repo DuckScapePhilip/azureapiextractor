f19a2852bf41e72559ee388afe8e9ba4ba88eff0
Rafael <rafael@pandoscape.com>
2024-05-28 21:00:30 +0100

    AnalyzeDocument created and ContentFormat edited to enum

---
+++ apis/azuredocumentintelligence/preview02292024/AzureDocumentIntelligenceAPI/Models/AnalyzeDocument/DocumentAnalysisFeature.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+
+namespace AzureDocumentIntelligenceAPI.Models.AnalyzeDocument
+{
+    /// <summary>
+    /// List of optional analysis features.
+    /// </summary>
+    public class DocumentAnalysisFeature
+    {
+        /// <summary>
+        /// Enable the detection of barcodes in the document.
+        /// </summary>
+        [JsonProperty("barcodes")]
+        public string? Barcodes { get; set; }
+
+        /// <summary>
+        /// Enable the detection of mathematical expressions in the document.
+        /// </summary>
+        [JsonProperty("formulas")]
+        public string? Formulas { get; set; }
+
+        /// <summary>
+        /// Enable the detection of general key value pairs (form fields) in the document.
+        /// </summary>
+        [JsonProperty("keyValuePairs")]
+        public string? KeyValuePairs { get; set; }
+
+        /// <summary>
+        /// Enable the detection of the text content language.
+        /// </summary>
+        [JsonProperty("languages")]
+        public string? Languages { get; set; }
+
+        /// <summary>
+        /// Perform OCR at a higher resolution to handle documents with fine print.
+        /// </summary>
+        [JsonProperty("ocrHighResolution")]
+        public string? OcrHighResolution { get; set; }
+
+        /// <summary>
+        /// Enable the extraction of additional fields via the queryFields query parameter.
+        /// </summary>
+        [JsonProperty("queryFields")]
+        public string? QueryFields { get; set; }
+
+        /// <summary>
+        /// Enable the recognition of various font styles.
+        /// </summary>
+        [JsonProperty("styleFont")]
+        public string? StyleFont { get; set; }
+    }
+}
--- apis/azuredocumentintelligence/preview02292024/AzureDocumentIntelligenceAPI/Models/ClassifyResult/ContentFormat.cs
+++ apis/azuredocumentintelligence/preview02292024/AzureDocumentIntelligenceAPI/Models/ClassifyResult/ContentFormat.cs
@@ -5,18 +5,16 @@ namespace AzureDocumentIntelligenceAPI.Models.ClassifyResult
     /// <summary>
     /// Format of the content in analyzed result.
     /// </summary>
-    public class ContentFormat
+    public enum ContentFormat
     {
         /// <summary>
         /// Markdown representation of the document content with section headings, tables, etc.
         /// </summary>
-        [JsonProperty("markDown")]
-        public string? MarkDown { get; set; }
+        MarkDown,
 
         /// <summary>
         /// Plain text representation of the document content without any formatting.
         /// </summary>
-        [JsonProperty("text")]
-        public string? Text { get; set; }
+        Text
     }
 }
