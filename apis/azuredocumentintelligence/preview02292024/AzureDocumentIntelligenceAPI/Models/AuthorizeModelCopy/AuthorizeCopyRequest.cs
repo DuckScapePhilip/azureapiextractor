fcb885eed0fd2601d788876958ac7dd9a6f25c53
Rafael <rafael@pandoscape.com>
2024-05-28 21:10:51 +0100

    Authorize Model Copy created

---
+++ apis/azuredocumentintelligence/preview02292024/AzureDocumentIntelligenceAPI/Models/AuthorizeModelCopy/AuthorizeCopyRequest.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace AzureDocumentIntelligenceAPI.Models.AuthorizeModelCopy
+{
+    /// <summary>
+    /// Request body to authorize document model copy.
+    /// </summary>
+    public class AuthorizeCopyRequest
+    {
+        /// <summary>
+        /// Document model description.
+        /// </summary>
+        [JsonProperty("description")]
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Unique document model name.
+        /// </summary>
+        [JsonProperty("modelId")]
+        public string? ModelId { get; set; }
+
+        /// <summary>
+        /// List of key-value tag attributes associated with the document model.
+        /// </summary>
+        [JsonProperty("tags")]
+        public object? Tags { get; set; }
+    }
+}
