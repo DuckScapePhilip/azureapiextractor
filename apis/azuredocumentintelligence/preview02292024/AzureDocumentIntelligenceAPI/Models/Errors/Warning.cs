049b5dad3eb4c7559828f2c9c78bfcf1f19d662c
Rafael <rafael@pandoscape.com>
2024-05-28 20:10:05 +0100

    ListClassifiers added

--- apis/azuredocumentintelligence/preview02292024/AzureDocumentIntelligenceAPI/Models/Errors/Warning.cs
+++ apis/azuredocumentintelligence/preview02292024/AzureDocumentIntelligenceAPI/Models/Errors/Warning.cs
@@ -1,6 +1,28 @@
-﻿namespace AzureDocumentIntelligenceAPI.Models.Errors
+﻿using Newtonsoft.Json;
+
+namespace AzureDocumentIntelligenceAPI.Models.Errors
 {
+    /// <summary>
+    /// The error object.
+    /// </summary>
     public class Warning
     {
+        /// <summary>
+        /// One of a server-defined set of warning codes.
+        /// </summary>
+        [JsonProperty("code")]
+        public string? Code { get; set; }
+
+        /// <summary>
+        /// A human-readable representation of the warning.
+        /// </summary>
+        [JsonProperty("message")]
+        public string? Message { get; set; }
+
+        /// <summary>
+        /// The target of the error.
+        /// </summary>
+        [JsonProperty("target")]
+        public string? Target { get; set; }
     }
 }
