76f71917e2e8e2ae3e2b1d5a68f9a35127eee038
Rafael <rafael@pandoscape.com>
2024-05-30 16:10:30 +0100

    GetDocumentClassifierBuildOperation created

---
+++ apis/azuredocumentintelligence/preview02292024/AzureDocumentIntelligenceAPI/Models/MiscellaneousOperations/GetDocumentClassifierBuildOperation/ContentSourceKind.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+
+namespace AzureDocumentIntelligenceAPI.Models.MiscellaneousOperations.GetDocumentClassifierBuildOperation
+{ 
+    /// <summary>
+    /// The kind of content source. Possible values include: 'azureBlob', 'azureBlobFileList', 'base64', 'url'.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ContentSourceKind
+    {
+        /// <summary>
+        /// Files in a path within an Azure Blob Storage container.
+        /// </summary>
+        azureBlob,
+
+        /// <summary>
+        /// A file list specifying individual files in an Azure Blob Storage container.
+        /// </summary>
+        azureBlobFileList,
+
+        /// <summary>
+        /// Content represented via Base64 encoding.
+        /// </summary>
+        base64,
+
+        /// <summary>
+        /// Content at a specific URL.
+        /// </summary>
+        url
+    }
+}
---
+++ apis/azuredocumentintelligence/preview02292024/AzureDocumentIntelligenceAPI/Models/MiscellaneousOperations/GetDocumentClassifierBuildOperation/ErrorResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace AzureDocumentIntelligenceAPI.Models.MiscellaneousOperations.GetDocumentClassifierBuildOperation
+{
+    /// <summary>
+    /// Error response object.
+    /// </summary>
+    public class ErrorResponse
+    {
+        /// <summary>
+        /// Error info.
+        /// </summary>
+        [JsonProperty("error")]
+        public Error? Error { get; set; }
+    }
+}
